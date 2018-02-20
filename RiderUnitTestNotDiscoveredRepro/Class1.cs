421d6a9490ab7446362db807647f20b832bd1d48
Fredrik Forssén <fredrik.forssen@paynova.com>
2018-02-20 12:19:48 +0100

    Initial commit

---
+++ RiderUnitTestNotDiscoveredRepro/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace RiderUnitTestNotDiscoveredRepro
+{
+    [TestFixture]
+    public class SomeRandomTests
+    {
+        [Test]
+        public void It_is_a_random_test()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
